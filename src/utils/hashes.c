08011f20699e6607875a3464c281a995705979bf
Zffu <radi0o0@outlook.com>
2024-11-01 22:50:31 +0100

    feat: added correct char* length getting

--- src/utils/hashes.c
+++ src/utils/hashes.c
@@ -2,9 +2,11 @@
  * Hashmap related utilities for Quickfall.
  */
 
+#include <string.h>
+
 /**
  * Generates a hash for a lexar token.
  */
-int tokenHash(char token[]) {
-    return sizeof(token);
+int tokenHash(char* token) {
+    return strlen(token);
 }
