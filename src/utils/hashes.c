c017f3c5399abf3108dd41925e012789e92e9783
Zffu <radi0o0@outlook.com>
2024-11-04 00:47:30 +0100

    feat: started reworking the hashing

--- src/utils/hashes.c
+++ src/utils/hashes.c
@@ -5,8 +5,17 @@
 #include <string.h>
 
 /**
- * Generates a hash for a lexar token.
+ * Generates a hash for a lexar token. Currently compromisable
  */
 int tokenHash(char* token) {
-    return strlen(token);
+    int len = strlen(token);
+    if(len == 1) {
+        return (int)(token[0]) - 40;
+    }
+
+    int hash = 0;
+    for(int i = 0; i < len; ++i) {
+        hash += (int)token[i] * (i / 10 + 0.1);
+    }
+    return hash;
 }
