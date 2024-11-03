1cde0f45bdaedcb4889a15c4d175a7f2cf74fae2
Zffu <radi0o0@outlook.com>
2024-11-04 00:08:49 +0100

    tests: updated lexer test

--- tests/lexer.c
+++ tests/lexer.c
@@ -4,17 +4,14 @@
 
 int main() {
     clock_t ts = clock();
-    struct LexerResult result = runLexer("function function function");
+    struct LexerResult result = runLexer("var function function");
     ts = clock() - ts;
 
     printf("\n\nLexer took %fs\n", ts / CLOCKS_PER_SEC);
 
     for(int i = 0; i < result.size + 1; ++i) {
-        printf("Token: t:%d raw:%s\n", result.tokens[i].type, result.tokens[i].value);
+        printf("Token: t:%d raw:%s'\n", result.tokens[i].type, result.tokens[i].value);
     }
 
-    scanf("%d");
-
     return 0;
-
 }
