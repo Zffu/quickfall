a99dbce5a4a95e3a6d94f132cff12757c27f2ca3
Zffu <radi0o0@outlook.com>
2024-11-04 15:09:34 +0100

    feat: Added opening and closing token types

--- src/lexer/tokens.c
+++ src/lexer/tokens.c
@@ -11,7 +11,13 @@
 enum TokenType {
     FUNCTION = 1,
     RETURN = 2,
-    VAR = 3
+    VAR = 3,
+    BRACKETS_OPEN = 4,
+    BRACKETS_CLOSE = 5,
+    PAREN_OPEN = 6,
+    PAREN_CLOSE = 7,
+    ARRAY_OPEN = 8,
+    ARRAY_CLOSE = 9
 };
 
 int types[highestTokenHash] = {};
