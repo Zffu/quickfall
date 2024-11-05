6de2da697bf1185c31b3a5253fd8ec75a7b6763f
Zffu <radi0o0@outlook.com>
2024-11-06 00:40:19 +0100

    tests: removed hash test, added a debug mode to file lexer test

--- tests/lexer.c
+++ tests/lexer.c
@@ -8,7 +8,7 @@
 
 int main(int argc, char* argv[]) {
     if(argc < 2) {
-        printf("Usage: <executable> <file>");
+        printf("Usage: <executable> <file> [debug]");
         return - 1;
     }
 
@@ -18,15 +18,29 @@ int main(int argc, char* argv[]) {
 
     FILE* filePtr = fopen(argv[1], "r");
 
-    printf("\nToken Analysis Test (Per Line):\n\n");
+    if(argc > 2) {
+        printf("\nToken Analysis Test (Debugging Mode):\n\n");
 
-    while(fgets(line, 128, filePtr) != NULL) {
-        struct LexerResult result = runLexer(line);
+        while(fgets(line, 128, filePtr) != NULL) {
+            struct LexerResult result = runLexer(line);
 
-        //printf("\nFound %d tokens:\n", result.size);        
-        for(int i = 0; i < result.size; ++i) {
-            //printf("Token -> type: %d, raw value: %s\n", result.tokens[i].type, result.tokens[i].value);
+            printf("\nFound %d tokens:\n", result.size);        
+            for(int i = 0; i < result.size; ++i) {
+                printf("Token -> type: %d, raw value: %s\n", result.tokens[i].type, result.tokens[i].value);
+            }
         }
+    }
+    else {
+        printf("\nToken Analysis Test:\n\n");
+
+        int count = 0;
+
+        while(fgets(line, 128, filePtr) != NULL) {
+            struct LexerResult result = runLexer(line);
+            count += result.size;
+        }
+
+        printf("Found %d tokens in file!", count);
     }
 
     ts = clock() - ts;
