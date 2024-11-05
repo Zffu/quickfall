8e9b466d6c6107e086910b770f8eaa9cd35b91c5
Zffu <radi0o0@outlook.com>
2024-11-06 00:51:30 +0100

    feat: added simple cli

---
+++ src/cli/main.c
@@ -0,0 +1,16 @@
+/**
+ * The main file of the Quickfall CLI.
+ */
+
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+    if(argc < 2) {
+        showHelpMessage();
+        return -1;
+    }
+}
+
+void showHelpMessage() {
+    printf("Quickfall commands:\n   quickfall -v - Shows the version information of your Quickfall distribution.\n   quickfall -l <file> - Runs a Quickfall lexer check on the provided file.");
+}
