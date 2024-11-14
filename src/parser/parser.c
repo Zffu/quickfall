8aaf5ba7261a4e2ab109787d6d77455a6760f890
Zffu <radi0o0@outlook.com>
2024-11-14 21:46:08 +0100

    feat: started working on a tree-ish parser

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -8,21 +8,42 @@
 #include "../lexer/tokens.h"
 
 /**
+ * A node of the parser, allows for a tree-ish visualization of the parser sub groups.
+ */
+struct ParserNode {
+    struct ParserNode* children;
+    struct Token* tokens;
+    int index;
+};
+
+/**
  * <p>Runs the parser of the provided LexerResult.
  */
 void runParser(struct LexerResult result) {
-    struct Token* content = malloc((result.size - 2) * sizeof(struct Token));
-    int inside = 0;
+    struct ParserNode root;
+    
+    struct ParserNode* branch = &root;
+    struct ParserNode* latest = &root;
 
     for(int i = 0; i < result.size; ++i) {
         struct Token token = result.tokens[i];
-        
+
         if(token.type == PAREN_OPEN || token.type == BRACKETS_OPEN || token.type == ARRAY_OPEN) {
-            inside = 1;
+            if(latest->children == NULL) {
+                latest->children = malloc(result.size - 2 * sizeof(struct Token)); //todo: optimise this
+            }
+            
+            latest->index++;
+            branch = latest;
+            latest = &latest->children[latest->index];
+        }
+
+        if(token.type == PAREN_CLOSE || token.type == BRACKETS_CLOSE || token.type == ARRAY_CLOSE) {
+            if(branch != NULL) {
+                latest = branch;
+            }
         }
     }
 
-    if(inside == 1) {
-        printf("Error: A token group wasn't closed before the end of the Token stream!");
-    }
+    printf("sjjduios");
 }
--- src/parser/parser.h
+++ src/parser/parser.h
@@ -8,6 +8,16 @@
 #include "../lexer/lexer.h"
 
 /**
+ * A node of the parser, allows for a tree-ish visualization of the parser sub groups.
+ */
+struct ParserNode {
+    struct ParserNode* parent;
+    struct ParserNode* children;
+    struct Token* tokens;
+    int index;
+};
+
+/**
  * <p>Runs the parser on the provided LexerResult.
  */
 void runParser(struct LexerResult result);
