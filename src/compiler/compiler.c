2ab8d8f7ee14204292d3b73f6941fa0c646a6f99
Zffu <radi0o0@outlook.com>
2024-11-16 23:27:02 +0100

    feat: added some compiler functions

--- src/compiler/compiler.c
+++ src/compiler/compiler.c
@@ -7,8 +7,8 @@
 #include "../parser/ast.h"
 
 void compile(struct ASTNode* node) {
-
-    char output[1024] = {"_start:"};
+    char sections[1024];
+    char startOutput[1024] = {"_start:"};
 
 
     while(node->next != NULL) {
@@ -20,14 +20,46 @@ void compile(struct ASTNode* node) {
                 strcat(instruction, node->right->next->value);
                 strcat(instruction, ", ");
                 strcat(instruction, node->right->next->next->value);
-                strcat(output, instruction);
+                strcat(startOutput, instruction);
             }
             else if(strcmp(node->left->value, "systemCall") == 0) {
                 char instruction[32] = {"\n    syscall"};
-                strcat(output, instruction);
+                strcat(startOutput, instruction);
+            }
+            else if(strcmp(node->left->value, "declareSect") == 0) {
+                char instruction[32] = {"section ."};
+                strcat(instruction, node->right->next->value);
+                strcat(sections, instruction);
+            }
+            else if(strcmp(node->left->value, "sectDeclare") == 0) {
+                char instruction[32] = {"\n    "};
+                strcat(instruction, node->right->next->value);
+                strcat(instruction, " ");
+                strcat(instruction, node->right->next->next->value);
+                strcat(instruction, " '");
+                strcat(instruction, node->right->next->next->next->value);
+                strcat(instruction, "', 0");
+                strcat(sections, instruction);
+            }
+            else if(strcmp(node->left->value, "registryXOR") == 0) {
+                strcat(startOutput, "\n    xor ");
+                strcat(startOutput, node->right->next->value);
+                strcat(startOutput, ", ");
+                strcat(startOutput, node->right->next->next->value);
+            }
+            else if(strcmp(node->left->value, "sectVoid") == 0) {
+                strcat(sections, "\n\n");
+            }
+            else if(strcmp(node->left->value, "sectGlobal") == 0) {
+                strcat(sections,"\n    global ");
+                strcat(sections, node->right->next->value);
             }
         }
     }
+
+    char output[2048];
+    strcat(output, sections);
+    strcat(output, startOutput);
 
     printf("\n%s", output);
 }
