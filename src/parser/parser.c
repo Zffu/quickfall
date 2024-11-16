b6c2b68f239370261fbf9fdadb27e8ffbf5f5ff6
Zffu <radi0o0@outlook.com>
2024-11-16 17:40:32 +0100

    feat: started working on an actual parser

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -2,58 +2,29 @@
  * The parser of Quickfall.
  */
 
-#include <stdio.h>
-#include <stdlib.h>
 #include "../lexer/lexer.h"
-#include "../lexer/tokens.h"
+#include <stdio.h>
 
-/**
- * A node of the parser, allows for a tree-ish visualization of the parser sub groups.
- */
-struct ParserNode {
-    struct ParserNode* children;
-    struct Token* tokens;
-    int childrenIndex;
-    int tokensIndex;
-};
-
-/**
- * <p>Runs the parser of the provided LexerResult.
- */
 void runParser(struct LexerResult result) {
-    struct ParserNode root;
-    
-    struct ParserNode* branch = &root;
-    struct ParserNode* latest = &root;
-
     for(int i = 0; i < result.size; ++i) {
-        struct Token token = result.tokens[i];
+        struct Token t = result.tokens[i];
+        struct Token next = result.tokens[i + 1];
 
-        switch(token.type) {
-            case PAREN_OPEN:
-            case BRACKETS_OPEN:
-            case ARRAY_OPEN:
-                if(latest->children == NULL) {
-                    latest->children = malloc((result.size - 2) * sizeof(struct Token));
-                }
+        if(t.type == FUNCTION) {
 
-                latest->childrenIndex++;
-                branch = latest;
-                latest = &latest->children[latest->childrenIndex];
-                break;
-            case PAREN_CLOSE:
-            case BRACKETS_CLOSE:
-            case ARRAY_CLOSE:
-                if(branch != NULL) {
-                    latest = branch;
-                }
-                break;
-            default:
-                latest->tokens[latest->tokensIndex] = token;
-                latest->tokensIndex++;
+            if(next.type == KEYWORD) {
+                parseFunctionDeclaration(result, i);
+            }
         }
-
     }
+}
 
-    printf("sjjduios");
+void parseFunctionDeclaration(struct LexerResult result, int index) {
+    for(int i = index + 3; i < result.size; ++i) {
+        struct Token t = result.tokens[i];
+
+        if(t.type == PAREN_CLOSE) break;
+
+        printf("Token in parameters: %d\n", t.type);
+    }
 }
--- src/parser/parser.h
+++ src/parser/parser.h
@@ -7,19 +7,6 @@
 
 #include "../lexer/lexer.h"
 
-/**
- * A node of the parser, allows for a tree-ish visualization of the parser sub groups.
- */
-struct ParserNode {
-    struct ParserNode* children;
-    struct Token* tokens;
-    int childrenIndex;
-    int tokensIndex;
-};
-
-/**
- * <p>Runs the parser on the provided LexerResult.
- */
 void runParser(struct LexerResult result);
 
 #endif
