084addde538038ec9f0059840de7ff3c2aeb9b2d
Zffu <radi0o0@outlook.com>
2024-11-20 22:31:51 +0100

    feat: added timing categories

--- benchmarks/main.c
+++ benchmarks/main.c
@@ -3,12 +3,29 @@
  */
 
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <time.h>
+#include <windows.h>
+
+LARGE_INTEGER frequency, start, end;
+long totalTime;
+
+long timeTaken[5];
+char* categories[] = {"File IO (Read)", "Lexer", "Parser (AST)", "Compiling", "File IO (Write)"};
+
 
 void main(int argc, char* argv[]) {
     if(argc < 2) {
         printf("Usage: <executable> <file> [runs, default = 100]");
         return;
+    }
+
+    int runs = 100;
+
+    if(argc > 2) {
+        runs = atoi(argv[2]);
     }
 
     FILE* fptr = fopen(argv[1], "r");
@@ -19,5 +36,10 @@ void main(int argc, char* argv[]) {
 
     char* buff = (char*) malloc(size);
     fread(buff, 1, size, fptr);
-    
+    fclose(fptr);
+
+    for(int i = 0; i < runs; ++i) {
+
+    }
+
 }
