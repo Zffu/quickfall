1dfd69c3127fa2118a32bdb5a198579d801dd55a
Zffu <radi0o0@outlook.com>
2024-11-20 23:12:29 +0100

    feat: added WIP benchmark

--- benchmarks/main.c
+++ benchmarks/main.c
@@ -5,15 +5,54 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include <time.h>
 #include <windows.h>
 
-LARGE_INTEGER frequency, start, end;
-long totalTime;
+#include "../src/lexer/lexer.h"
+#include "../src/parser/parser.h"
+#include "../src/parser/ast.h"
+#include "../src/compiler/compiler.h"
 
-long timeTaken[5];
-char* categories[] = {"File IO (Read)", "Lexer", "Parser (AST)", "Compiling", "File IO (Write)"};
+//
+//
+// Timing Utilities
+//
+//
+
+float totalTimeTaken;
+float* timeTaken;
+
+LARGE_INTEGER frequency, start, end;
+
+int categoriesSize;
+char** categories;
+
+/**
+ * Sets the categories for the Quickfall Benchmarking Context.
+ */
+void setCategories(int amount, char* categories[]) {
+    categories = categories;
+    timeTaken = malloc(amount * sizeof(float));
+    categoriesSize = amount;
+}
+
+/**
+ * Starts the monitoring
+ */
+void startMonitoring() {
+    QueryPerformanceFrequency(&frequency);
+    QueryPerformanceCounter(&start);
+}
+
+/**
+ * Stops monitoring the current benchmark context.
+ */
+void stopMonitoring(int category) {
+    QueryPerformanceCounter(&end);
+    float taken = (end.QuadPart - start.QuadPart) * 1000000.0 / frequency.QuadPart;
+
+    totalTimeTaken += taken;
+    timeTaken[category] += taken;
+}
 
 
 void main(int argc, char* argv[]) {
@@ -28,18 +67,54 @@ void main(int argc, char* argv[]) {
         runs = atoi(argv[2]);
     }
 
-    FILE* fptr = fopen(argv[1], "r");
-
-    fseek(fptr, 0, SEEK_END);
-    int size = ftell(fptr);
-    fseek(fptr, 0, SEEK_SET);
-
-    char* buff = (char*) malloc(size);
-    fread(buff, 1, size, fptr);
-    fclose(fptr);
+    char* categories[] = {"File IO (Open)", "Lexer", "Parser", "Compiler", "File IO (Close)"};
+    setCategories(5, categories);
 
     for(int i = 0; i < runs; ++i) {
+        startMonitoring();
+        FILE* fptr = fopen(argv[1], "r");
 
+        fseek(fptr, 0, SEEK_END);
+        int size = ftell(fptr);
+        fseek(fptr, 0, SEEK_SET);
+
+        char* buff = (char*) malloc(size);
+        fread(buff, 1, size, fptr);
+        fclose(fptr);
+
+        stopMonitoring(0);
+        startMonitoring();
+
+        struct LexerResult result = runLexer(buff);
+
+        stopMonitoring(1);
+
+        startMonitoring();
+
+        struct ASTNode* node = runParser(result);
+
+        stopMonitoring(2);
+        startMonitoring();
+
+        struct CompilerOutput compiled = compile(node, "win");
+
+        stopMonitoring(3);
+
+        char* output = argv[2];
+        strcat(output, ".build");
+
+        startMonitoring();
+
+        fptr = fopen(output, "w");
+        fprintf(fptr, compiled.output);
+
+        stopMonitoring(4);
     }
 
+    printf("========= Benchmarking Results =========\n");
+    
+    for(int i = 0; i < 5; ++i) {
+        float taken = timeTaken[i] / runs;
+        printf("%s: %fμs (%d%)\n", categories[i], taken, ((totalTimeTaken / runs) / taken) * 100);
+    }
 }
