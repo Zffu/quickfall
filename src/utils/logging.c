efdf3f7a644dc2d13e5f75c76d48d2bba58f7d3e
Zffu <radi0o0@outlook.com>
2024-11-23 03:38:54 +0100

    feat: started working on logging utilities

---
+++ src/utils/logging.c
@@ -0,0 +1,14 @@
+/**
+ * Quickfall Logging Utilities.
+ */
+
+#define RESET "\x1b[0m"
+
+#define TEXT_BLACK "\x1b[30m"
+#define TEXT_RED "\x1b[31m"
+#define TEXT_GREEN "\x1b[32m"
+#define TEXT_YELLOW "\x1b[33m"
+#define TEXT_BLUE "\x1b[34m"
+#define TEXT_MAGENTA "\x1b[35m"
+#define TEXT_CYAN "\x1b[36m"
+#define TEXT_WHITE "\x1b[37m"
