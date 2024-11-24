55a9674b8b8c5aa7eb5dd5aa2bd7778680724451
Zffu <radi0o0@outlook.com>
2024-11-24 13:10:19 +0100

    feat: added setCategories

--- benchmarks/core/bench.h
+++ benchmarks/core/bench.h
@@ -5,3 +5,10 @@
 char** categories;
 
 long* timeTaken;
+
+/**
+ * Sets the active benchmarking categories.
+ * @param count the count of categories in the array.
+ * @param the category array.
+ */
+void setCategories(int count, char* categories[]) {};
