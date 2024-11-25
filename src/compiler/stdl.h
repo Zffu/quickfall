27d3c033e40bf2eb0f76bb134f612cd25a9fd2e1
Zffu <radi0o0@outlook.com>
2024-11-25 01:21:12 +0100

    feat: added loadAndDump function in header

---
+++ src/compiler/stdl.h
@@ -0,0 +1,12 @@
+/**
+ * The code in charge of managing the standart library usage in Quickfall.
+ */
+
+#include "./compiler.h"
+
+/**
+ * Loads a component (a file) of the standart library and dump its content into the assembly output.
+ * @param ctx the compiling context of the currently running compiling.
+ * @param component the component name (file name) of the part to load.
+ */
+void loadAndDump(struct CompilingContext ctx, char* component);
