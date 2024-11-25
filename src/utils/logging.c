b9b8963b1b827ac46f1f45fbf80dddc84338813a
Zffu <radi0o0@outlook.com>
2024-11-25 03:26:14 +0100

    feat: started reworking color variants

--- src/utils/logging.c
+++ src/utils/logging.c
@@ -4,25 +4,48 @@
 
 #define RESET "\x1b[0m"
 
-#define TEXT_BLACK "\x1b[30m"
-#define TEXT_RED "\x1b[31m"
-#define TEXT_GREEN "\x1b[32m"
-#define TEXT_YELLOW "\x1b[33m"
-#define TEXT_BLUE "\x1b[34m"
-#define TEXT_MAGENTA "\x1b[35m"
-#define TEXT_CYAN "\x1b[36m"
-#define TEXT_WHITE "\x1b[37m"
-#define TEXT_GRAY "\033[90m"
+#define PREFIX "\x1b["
 
-#define BACKGROUND_BLACK "\x1b[40m"
-#define BACKGROUND_RED "\x1b[41m"
-#define BACKGROUND_GREEN "\x1b[42m"
-#define BACKGROUND_YELLOW "\x1b[43m"
-#define BACKGROUND_BLUE "\x1b[44m"
-#define BACKGROUND_MAGENTA "\x1b[45m"
-#define BACKGROUND_CYAN "\x1b[46m"
-#define BACKGROUND_WHITE "\x1b[47m"
+#define CLASSIC_OP PREFIX "0;"
+#define BOLD_OP PREFIX "1;"
 
-#define STYLE_BOLD "\x1b[1m"
-#define STYLE_ITALIC "\x1b[3m"
-#define STYLE_UNDERLINE "\x1b[4m"
+#define RED "31m"
+#define BLACK "30m"
+#define GREEN "32m"
+#define YELLOW "33m"
+#define BLUE "34m"
+#define MAGENTA "35m"
+#define CYAN "36m"
+#define WHITE "37m"
+#define GRAY "90m"
+
+#define BG_BLACK "40m"
+#define BG_RED "41m"
+#define BG_GREEN "42m"
+#define BG_YELLOW "43m"
+#define BG_BLUE "44m"
+#define BG_MAGENTA "45m"
+#define BG_CYAN "46m"
+#define BG_WHITE "47m"
+#define BG_GRAY "48m"
+
+#define TEXT_RED CLASSIC_OP RED
+#define TEXT_BLACK CLASSIC_OP BLACK
+#define TEXT_GREEN CLASSIC_OP GREEN
+#define TEXT_YELLOW CLASSIC_OP YELLOW
+#define TEXT_BLUE CLASSIC_OP BLUE
+#define TEXT_MAGENTA CLASSIC_OP MAGENTA
+#define TEXT_CYAN CLASSIC_OP CYAN
+#define TEXT_WHITE CLASSIC_OP WHITE
+#define TEXT_GRAY CLASSIC_OP GRAY
+
+#define BACKGROUND_RED CLASSIC_OP BG_RED
+#define BACKGROUND_BLACK CLASSIC_OP BG_BLACK
+#define BACKGROUND_GREEN CLASSIC_OP BG_GREEN
+#define BACKGROUND_YELLOW CLASSIC_OP BG_YELLOW
+#define BACKGROUND_BLUE CLASSIC_OP BG_BLUE
+#define BACKGROUND_MAGENTA CLASSIC_OP BG_MAGENTA
+#define BACKGROUND_CYAN CLASSIC_OP BG_CYAN
+#define BACKGROUND_WHITE CLASSIC_OP BG_WHITE
+#define BACKGROUND_GRAY CLASSIC_OP BG_GRAY
+
