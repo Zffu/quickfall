27fa9264d1d2d2b85971b933af4aa6fd50220a62
Zffu <radi0o0@outlook.com>
2024-11-29 00:55:13 +0100

    feat: added parserContext

--- src/compilerv2/compilerv2.h
+++ src/compilerv2/compilerv2.h
@@ -5,11 +5,18 @@
 #ifndef COMPILER_2_H
 #define COMPILER_2_H
 
+#include "../parser/ast.h"
+
 /**
  * A context is the less abstract way Quickfall represents the code before converting it to assembly.
  */
 struct Context {
 
 };
+
+/**
+ * Parses the AST tree into a context.
+ */
+struct Context parseContext(struct ASTNode* node);
 
 #endif
