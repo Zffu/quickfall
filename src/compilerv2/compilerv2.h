fd31f685d47a1518c6562fdae344fd337605ed7f
Zffu <radi0o0@outlook.com>
2024-11-29 00:53:58 +0100

    feat: added empty header

---
+++ src/compilerv2/compilerv2.h
@@ -0,0 +1,15 @@
+/**
+ * The compiler of Quickfall.
+ */
+
+#ifndef COMPILER_2_H
+#define COMPILER_2_H
+
+/**
+ * A context is the less abstract way Quickfall represents the code before converting it to assembly.
+ */
+struct Context {
+
+};
+
+#endif
