75c0271270d7c39bb42e433d6933775e603c4580
Zffu <radi0o0@outlook.com>
2024-11-29 01:01:07 +0100

    feat: added compiler objects

---
+++ src/compilerv2/objects.h
@@ -0,0 +1,30 @@
+/**
+ * Compiling objects such as functions, variables...
+ */
+
+#ifndef COMPILER_OBJECTS_H
+#define COMPILER_OBJECTS_H
+
+#include "../parser/ast.h"
+
+struct Variable {
+	char* name;
+	char* value;
+	char* type;
+	byte type; // 0 = variable, 1 = Func argument, 2 = Function body argument
+};
+
+struct FunctionBody {
+	struct Variable* 
+};
+
+struct Function {
+	char* name;
+	struct Variable* variables;
+	int variableCount;
+	struct ASTNode* body;
+};
+
+
+
+#endif
