216310fcb8c3715e7f594003bb8cf8e669b77954
Zffu <radi0o0@outlook.com>
2024-12-01 20:45:16 +0100

    feat: added attlinux assembly format

---
+++ src/compilerv2/att/att-linux.h
@@ -0,0 +1,13 @@
+/**
+ * The Linux-AT & T assembly format.
+ */
+
+/**
+ * The registries used for function arguments.
+ */
+char* ATTLINUX_ARGUMENT_REGISTRIES[] = {"rdi", "rsi", "rdx", "rcx", "r8", "r9"};
+
+/**
+ * The types of different data values that are registered in sections and not the stack.
+ */
+char* ATTLINUX_SECTION_TYPES[] = {".string"};
