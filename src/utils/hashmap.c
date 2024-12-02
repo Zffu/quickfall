f3d13193b07d01296182b77310953ca77ffe0edf
Zffu <radi0o0@outlook.com>
2024-12-02 22:23:00 +0100

    feat: added hashmap

---
+++ src/utils/hashmap.c
@@ -0,0 +1,54 @@
+/**
+ * A simple hashmap implementation.
+ */
+
+#include <stdlib.h>
+
+struct Bucket {
+	void** values;
+	int bucketSize;
+};
+
+struct Hashmap {
+	int bucketSize;
+	int maxBucketIndex;
+
+	struct Bucket** buckets;
+};
+
+struct Hashmap* createHashmap(int bucketSize, int maxBucketIndex) {
+	struct Hashmap* map = malloc(sizeof(struct Hashmap));
+	map->bucketSize = bucketSize;
+	map->maxBucketIndex = maxBucketIndex;
+
+	map->buckets = malloc(sizeof(struct Bucket*) * maxBucketIndex);
+	
+	for(int i = 0; i < maxBucketIndex; ++i) {
+		map->buckets[i] = NULL;
+	}
+
+	return map;
+}
+
+void hashPut(struct Hashmap* hashmap, int hash, void* value) {
+	int bucket = hash / hashmap->bucketSize;
+
+	if(hashmap->buckets[bucket] == NULL) {
+		struct Bucket* b = malloc(sizeof(struct Bucket));
+		b->bucketSize = hashmap->bucketSize;
+		b->values = malloc(sizeof(int*) * b->bucketSize);
+
+		hashmap->buckets[bucket] = b;
+	}
+
+	hashmap->buckets[bucket]->values[hash % hashmap->bucketSize] = value;
+}
+
+void* hashGet(struct Hashmap* hashmap, int hash) {
+	int bucket = hash / hashmap->bucketSize;
+	
+	if(hashmap->buckets[bucket] == NULL) return NULL;
+
+	return hashmap->buckets[bucket]->values[hash % hashmap->bucketSize];
+}
+
