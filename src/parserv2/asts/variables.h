46f1e1694ec328af522005043862b1a57be32168
Zffu <radi0o0@outlook.com>
2024-12-05 23:14:51 +0100

    feat: added variable asts header

---
+++ src/parserv2/asts/variables.h
@@ -0,0 +1,17 @@
+/**
+ * Variable-related AST parsing.
+ */
+
+#include "../ast.h"
+
+#ifndef AST_VARIABLES_H
+#define AST_VARIABLES_H
+
+/**
+ * Parses a variable value.
+ * @param result the lexer result.
+ * @param index the starting index.
+ */
+AST_NODE* parseVariableValue(struct LexerResult result, int index);
+
+#endif
