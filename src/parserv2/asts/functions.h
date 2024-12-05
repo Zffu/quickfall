88712cb15ce2cb6cd41adf932cab60f8836453af
Zffu <radi0o0@outlook.com>
2024-12-05 22:59:44 +0100

    feat: started adding asts

---
+++ src/parserv2/asts/functions.h
@@ -0,0 +1,25 @@
+/**
+ * Function-related AST parsing.
+ */
+
+#include "../ast.h"
+
+#ifndef AST_FUNC_H
+#define AST_FUNC_H
+
+/**
+ * Parse the parameters from a function defintition (for example).
+ * @param result the lexer result.
+ * @param index the starting index of the parsing.
+ */
+AST_NODE* parseParameters(struct LexerResult result, int index);
+
+/**
+ * Parses the arguments from a function call (for example).
+ * @param result the lexer result.
+ * @param index the starting index of the parsing.
+ */
+AST_NODE* parseArguments(struct LexerResult result, int index);
+
+
+#endif
