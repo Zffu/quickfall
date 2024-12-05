878fbb64d5645bcb62d466f99dd4725cdf498ff2
Zffu <radi0o0@outlook.com>
2024-12-05 22:51:39 +0100

    feat: createASTNode implementation

---
+++ src/parserv2/ast.c
@@ -0,0 +1,18 @@
+/**
+ * The AST Nodes in Quickfall.
+ */
+
+#include <stdlib.h>
+
+#include "./ast.h"
+
+/**
+ * Creates a new AST Node.
+ * @param type the AST type of the node.
+ */
+AST_NODE* createASTNode(enum ASTNodeType type) {
+	AST_NODE* node = malloc(sizeof(AST_NODE));
+	node->type = type;
+
+	return node;
+}
