b9e5d1db8b874ed869455709ee4f5a122f549738
Zffu <radi0o0@outlook.com>
2024-12-10 20:31:19 +0100

    feat: added math asts header

---
+++ src/parser/asts/math.h
@@ -0,0 +1,13 @@
+/**
+ * Math ASTs for the Quickfall parser.
+ */
+
+#include "../../ast.h"
+
+/**
+ * Parses the mathematical operation.
+ * @param result the lexer result.
+ * @param index the starting index.
+ */
+AST_NODE* parseMathematicalOpNode(struct LexerResult result, int index);
+
