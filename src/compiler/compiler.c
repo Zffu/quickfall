d0a66cbc31b405c66869f409710d35d0d6b1d4a5
Zffu <radi0o0@outlook.com>
2024-12-15 10:53:31 +0100

    chore: removed useless imports in compiler header impl

--- src/compiler/compiler.c
+++ src/compiler/compiler.c
@@ -2,13 +2,9 @@
  * The compiler of Quickfall.
  */
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-
 #include "./compiler.h"
 #include "./ir.h"
 
 #include "../parser/ast.h"
-#include "../utils/logging.c"
+
 
