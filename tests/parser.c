f7259f7662d69d4c28c2e170142b4d79241eae4e
Zffu <radi0o0@outlook.com>
2024-12-15 11:49:56 +0100

    tests: implemented parser test

--- tests/parser.c
+++ tests/parser.c
@@ -12,33 +12,12 @@
 
 void dumpASTTree(AST_NODE* node, int depth);
 
-int main(int argc, char* argv[]) {
-	if(argc < 2) {
-		printf("Usage: <exec> <file>\n");
-		return -1;
-	}
-
-	FILE* fptr = fopen(argv[1], "r");
-
-	if(fptr == NULL) {
-		printf("Couldn't open file!\n");
-		return -1;
-	}
-
-	fseek(fptr, 0, SEEK_END);
-	int size = ftell(fptr);
-	fseek(fptr, 0, SEEK_SET);
-
-	char* buff = malloc(size + 1);
-
-	fread(buff, 1, size, fptr);
-	buff[size] = '\0';
-	fclose(fptr);
-
+inline int runParserTest(char* buff) {
 	struct LexerResult result = runLexer(buff);
-	struct ASTNode* root = parseNodes(result, 0, AST_ROOT);
 
-	dumpASTTree(root, 0);
+	AST_NODE* root = parseNodes(buff, 0, AST_ROOT);
+
+	dumpASTTree(node, 0);
 }
 
 char* debug[12] = {"Root", "Type Node", "Variable Name", "Variable Value", "Variable Declaration", "Variable Reference", "Function Declaration", "Function Header", "Math Operator", "Math Operation", "Math Operation Header", "Parameter"};
