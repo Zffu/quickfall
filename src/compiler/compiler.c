68e75ff46d0427c9d1c20be18b9799433dcb5e89
Zffu <radi0o0@outlook.com>
2024-12-15 10:55:00 +0100

    feat: added empty sample compiler header func impls

--- src/compiler/compiler.c
+++ src/compiler/compiler.c
@@ -7,4 +7,19 @@
 
 #include "../parser/ast.h"
 
+/**
+ * Parses the AST tree into a context.
+ * @param tree the AST tree.
+ */
+IR_CTX* makeContext(AST_NODE* tree) {
+	
+}
 
+/**
+ * Compiles the Context tree to an executable named the provided file name.
+ * @param ctx the IR context.
+ * @param char the output file name.
+ */
+void compile(IR_CTX* ctx, char* outputFileName) {
+
+}
