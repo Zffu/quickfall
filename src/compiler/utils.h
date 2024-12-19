8228e15e1d86303f8d9014d9a0254d302f828359
Zffu <zffumc@gmail.com>
2024-12-19 12:02:47 +0100

    feat: added write8, write16 and write32 into compiler utils header

--- src/compiler/utils.h
+++ src/compiler/utils.h
@@ -2,7 +2,13 @@
  * Utilities for the Quickfall Compiler.
  */
 
+#include <stdint.h>
+
 #ifndef COMPILER_UTILS_H
 #define COMPILER_UTILS_H
+
+void write8(FILE* fptr, uint8_t value);
+void write16(FILE* fptr, uint16_t value);
+void write32(FILE* fptr, uint32_t value);
 
 #endif
