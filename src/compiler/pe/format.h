f35c27327aac5326dcef3a91431886a73f6a9204
Zffu <radi0o0@outlook.com>
2024-12-22 14:50:01 +0100

    feat: added PE_HEADER

--- src/compiler/pe/format.h
+++ src/compiler/pe/format.h
@@ -39,4 +39,54 @@ typdef struct PE_DOS_HEADER {
     	long   e_lfanew;                    // File address of new exe header
 } PE_DOS_HEADER;
 
+typedef struct PE_FILE_HEADER {
+	WORD machine;
+	WORD sec_num;
+	DWORD timestamp;
+	DWORD symbolTablePtr;
+	DWORD symbolCount;
+	WORD optionalHeaderSize;
+	WORD chrs;
+} PE_FILE_HEADER;
+
+typdef struct PE_OPTIONAL_HEADER {
+	WORD                 Magic;
+  	BYTE                 MajorLinkerVersion;
+  	BYTE                 MinorLinkerVersion;
+  	DWORD                SizeOfCode;
+  	DWORD                SizeOfInitializedData;
+  	DWORD                SizeOfUninitializedData;
+  	DWORD                AddressOfEntryPoint;
+  	DWORD                BaseOfCode;
+  	DWORD                BaseOfData;
+  	DWORD                ImageBase;
+  	DWORD                SectionAlignment;
+  	DWORD                FileAlignment;
+  	WORD                 MajorOperatingSystemVersion;
+  	WORD                 MinorOperatingSystemVersion;
+  	WORD                 MajorImageVersion;
+  	WORD                 MinorImageVersion;
+  	WORD                 MajorSubsystemVersion;
+  	WORD                 MinorSubsystemVersion;
+  	DWORD                Win32VersionValue;
+  	DWORD                SizeOfImage;
+  	DWORD                SizeOfHeaders;
+  	DWORD                CheckSum;
+  	WORD                 Subsystem;
+  	WORD                 DllCharacteristics;
+	DWORD                SizeOfStackReserve;
+ 	DWORD                SizeOfStackCommit;
+  	DWORD                SizeOfHeapReserve;
+  	DWORD                SizeOfHeapCommit;
+  	DWORD                LoaderFlags;
+  	DWORD                NumberOfRvaAndSizes;
+} PE_OPTIONAL_HEADER;
+
+typedef struct PE_HEADER {
+	DWORD signature;
+	PE_FILE_HEADER file_header;
+	PE_OPTIONAL_HEADER optional_header;
+} PE_HEADER;
+
+
 #endif
