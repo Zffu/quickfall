720c21cbb93c0d66b745cb5e4bc992d411e47d53
Zffu <radi0o0@outlook.com>
2024-12-22 14:23:58 +0100

    feat: added compiler PE format datatypes

---
+++ src/compiler/win.h
@@ -0,0 +1,14 @@
+/**
+ * Quickfall Windows-Compiling (PE format).
+ */
+
+/**
+ * PE format data types.
+ */
+typedef DWORD unsigned long
+typedef WORD unsigned int
+
+#ifndef COMPILER_WIN
+#define COMPILER_WIN
+
+#endif
