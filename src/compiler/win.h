7b4a80af84d42451d8d320b3e7a292be327bba1d
Zffu <radi0o0@outlook.com>
2024-12-22 14:31:32 +0100

    fix: fixed WORD and added PE_DOS_HEADER

--- src/compiler/win.h
+++ src/compiler/win.h
@@ -9,6 +9,33 @@
  * PE Format datatypes.
  */
 typedef DWORD unsigned long
-typedef WORD unsigned int
+typedef WORD unsigned short
+
+/**
+ * PE Format Structure Defintions.
+ */
+//todo: we could use Windows bundled structures but that wouldn't be cross platform compiling.
+
+typedef struct PE_DOS_HEADER {
+	WORD e_magic;
+	WORD e_cblp;
+	WORD e_cp;
+	WORD e_crlc;
+	WORD e_cpardhr;
+	WORD e_minalloc;
+	WORD e_maxalloc;
+	WORD e_ss;
+	WORD e_sp;
+	WORD e_csum;
+	WORD e_ip;
+	WORD e_cs;
+	WORD e_lfarlc;
+	WORD e_ovno;
+	WORD e_res[4];
+	WORD e_oemid;
+	WORD e_oeminfo;
+	WORD e_res2[4];
+	long e_lfanew;
+} DOS_HEADER;
 
 #endif
