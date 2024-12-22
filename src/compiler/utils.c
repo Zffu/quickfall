e07d3e797e5cb4b7ef2f7777db279caa16f60e19
Zffu <radi0o0@outlook.com>
2024-12-22 14:18:04 +0100

    chore: removed old not working windows pe format

--- src/compiler/utils.c
+++ src/compiler/utils.c
@@ -2,16 +2,22 @@
  * Utilities for the Quickfall compiler.
  */
 
+#include <assert.h>
 #include <string.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 
 /**
  * Writes an 8 bit number into the file.
  */
 void write8(FILE *file, uint8_t value) {
-	if (fputc(value, file) == EOF) {
+
+	printf("Input %d for: %02x\n", value, (value >> 0) & 0xff);
+
+        if (fputc(value, file) == EOF) {
                 perror("fputc");
+                exit(1);
         }
 }
 
@@ -31,6 +37,8 @@ void write32(FILE *file, uint32_t value) {
         write8(file, (value >> 8)  & 0xff);
         write8(file, (value >> 16) & 0xff);
         write8(file, (value >> 24) & 0xff);
+
+	printf("32 Input for %d: %02x %02x %02x %02x\n", (value >> 0) & 0xff, (value >> 8) & 0xff, (value >> 16) && 0xff, (value >> 24) & 0xff);
 }
 
 /**
@@ -40,6 +48,7 @@ void writestr8(FILE *file, char *str) {
         size_t len, i;
 
         len = strlen(str);
+        assert(len <= 8 && "The string must fit in 8 bytes.");
 
         for (i = 0; i < 8; i++) {
                 write8(file, i < len ? str[i] : 0);
@@ -53,6 +62,7 @@ void writestr16(FILE *file, char *str) {
         size_t len, i;
 
         len = strlen(str);
+        assert(len <= 15 && "The string and terminator must fit in 16 bytes.");
 
         for (i = 0; i < 16; i++) {
                 write8(file, i < len ? str[i] : 0);
@@ -65,6 +75,7 @@ void writestr16(FILE *file, char *str) {
 void seek(FILE *file, long offset) {
         if (fseek(file, offset, SEEK_SET) == -1) {
                 perror("fseek");
+                exit(1);
         }
 }
 
