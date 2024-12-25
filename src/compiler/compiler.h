e8c280f9d9328b1b50af89d81f3e389a83795861
Zffu <radi0o0@outlook.com>
2024-12-25 15:08:40 +0100

    feat: removed old IR

--- src/compiler/compiler.h
+++ src/compiler/compiler.h
@@ -8,19 +8,11 @@
 #include "../utils/hashmap.h"
 #include "../parser/ast.h"
 
-#include "./ir.h"
-
-/**
- * Parses the AST tree into a context.
- * @param tree the AST tree.
- */
-IR_CTX* makeContext(AST_NODE* tree);
-
 /**
  * Compiles the Context tree to an executable named the provided name.
  * @param ctx the IR context.
  * @param char the output file.
  */
-void compile(IR_CTX* ctx, FILE* outputFileName);
+void compile(FILE* outputFileName);
 
 #endif
