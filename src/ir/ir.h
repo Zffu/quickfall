32f1dd08d960578a384dc866e34eb36d1f0d3d7f
Zffu <radi0o0@outlook.com>
2024-12-25 15:06:30 +0100

    feat: added block struct

--- src/ir/ir.h
+++ src/ir/ir.h
@@ -17,4 +17,14 @@ typedef struct {
 
 } IR_INSTRUCTION;
 
+/**
+ * An IR basic block.
+ */
+typedef struct {
+
+    IR_INSTRUCTION* instructions;
+    int instructionCount;
+
+} IR_BASIC_BLOCK;
+
 #endif
