1c2b8e5516c0ab5b0b9de38eb70af1896d9c5e6f
Zffu <radi0o0@outlook.com>
2024-12-25 15:23:50 +0100

    feat: added BLOCK_SWAP ir instruction

---
+++ src/ir/instructions.h
@@ -0,0 +1,17 @@
+/**
+ * The instruction codes for the Quickfall IR.
+ */
+
+#ifndef IR_INSTRUCTIONS
+#define IR_INSTRUCTIONS
+
+/**
+ * The instruction codes of IR.
+ */
+typedef enum {
+
+    BLOCK_SWAP
+
+} IR_INSTRUCTION_CODES;
+
+#endif
