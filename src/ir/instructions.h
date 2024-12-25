9c6f8555b8416144a2a7ae9f3feb8775233bd540
Zffu <radi0o0@outlook.com>
2024-12-25 15:52:56 +0100

    feat: added docs for block swapping instructions

--- src/ir/instructions.h
+++ src/ir/instructions.h
@@ -10,9 +10,27 @@
  */
 typedef enum {
 
+    /**
+     * Swaps the current IR block.
+     * @param block the new IR block index in the context (function for instance).
+     */
     BLOCK_SWAP,
+
+    /**
+     * Swaps the current IR block if a condition is met.
+     * @param block the new IR block index in the context (function for instance).
+     * @param cond the condition to match.
+     */
     COND_BLOCK_SWAP,
+
+    /**
+     * Swaps the current IR block depending on a condition result.
+     * @param trueBlock the new IR block index in the context (function for instance) if the condition is met.
+     * @param falseBlock the new IR block index in the context (function for instance) if the condition isn't met.
+     * @param cond the condition.
+     */
     LOGICAL_BLOCK_SWAP,
+
 
     S_ALLOC,
     PTR_SET,
