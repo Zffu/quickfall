9257bf5050078644cf7007bd3c69c705418f311c
Zffu <radi0o0@outlook.com>
2024-12-26 15:54:01 +0100

    feat: added parseVariableDeclaration in header

---
+++ src/parser/asts/variables.h
@@ -0,0 +1,19 @@
+/**
+ * Parsing for variable related ASTs.
+ */
+
+#include "../structs/variables.h"
+
+#include "../../lexer/lexer.h"
+
+#ifndef VARIABLES_ASTS_H
+#define VARIABLES_ASTS_H
+
+/**
+ * Parses a variable declaration.
+ * @param result the Lexer result.
+ * @param index the index where the parsing needs to start.
+ */
+AST_VARIABLE_DEC parseVariableDeclaration(LEXER_RESULT result, int index);
+
+#endif
