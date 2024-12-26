8759b26b3c6c6c6ee081ce4f19515d0225e056fa
Zffu <radi0o0@outlook.com>
2024-12-26 16:00:32 +0100

    feat: added parseVariableModification in header

--- src/parser/asts/variables.h
+++ src/parser/asts/variables.h
@@ -16,4 +16,11 @@
  */
 AST_VARIABLE_DEC parseVariableDeclaration(LEXER_RESULT result, int index);
 
+/**
+ * Parses a variable modification.
+ * @param result the Lexer result.
+ * @param index the index where the parsing needs to start.
+ */
+AST_VARIABLE_MOD parseVariableModification(LEXER_RESULT result, int index);
+
 #endif
