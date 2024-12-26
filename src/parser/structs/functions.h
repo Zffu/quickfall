4e06b34c86920b42b3488c06d7ac04e2576e7ad1
Zffu <radi0o0@outlook.com>
2024-12-26 13:37:06 +0100

    feat: added function dec struct

---
+++ src/parser/structs/functions.h
@@ -0,0 +1,21 @@
+/**
+ * AST structure definitions for function related ASTs.
+ */
+
+#ifndef FUNCTIONS_STRUCT_H
+#define FUNCTIONS_STRUCT_H
+
+/**
+ * The function declaration structure.
+ */
+typedef struct {
+    unsigned char type; // The type of the AST node (unused.)
+
+    char* funcName;
+    unsigned char* returnType;
+
+    void* body;
+
+} AST_FUNCTION_DEC;
+
+#endif
--- src/parser/structs/tree.h
+++ src/parser/structs/tree.h
@@ -1,3 +1,7 @@
+/**
+ * AST structure definitions for Tree related ASTs.
+ */
+
 #ifndef TREE_STRUCT_H
 #define TREE_STRUCT_H
 
@@ -5,7 +9,7 @@
  * The main AST root tree structure.
  */
 typedef struct {
-    unsigned char type;
+    unsigned char type; // The type of the AST node type (unused).
 
     void* next; // Refers to the next node in the tree.
     void* last; // Refers to the furthest / last node in the tree.
