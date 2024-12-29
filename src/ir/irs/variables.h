a4137f48a4bda28ffd10016fffec172ac4ba016e
Zffu <radi0o0@outlook.com>
2024-12-29 15:33:24 +0100

    feat: started moving the ir parsing into thier respective categories

---
+++ src/ir/irs/variables.h
@@ -0,0 +1,20 @@
+/**
+ * IR for variable related.
+ */
+
+#include "../../parser/structs/variables.h"
+
+#include "../ir.h"
+
+#ifndef IR_VARIABLES_H
+#define IR_VARIABLES_H
+
+/**
+ * Parses a variable declaration.
+ * @param block the IR basic block to append to.
+ * @param node the AST node representing the variable.
+ */
+inline void parseVariableDeclaration(IR_BASIC_BLOCK block, AST_VARIABLE_DEC* node);
+
+
+#endif
