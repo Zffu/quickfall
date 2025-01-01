dc7a289df817b528d5c99658ecea5d12aea1683b
Zffu <radi0o0@outlook.com>
2025-01-01 17:43:41 +0100

    feat: added instruction hash -> code type

--- src/qasm/parser.c
+++ src/qasm/parser.c
@@ -10,6 +10,9 @@
 
 #include "../ir/structs.h"
 #include "../ir/ir.h"
+#include "../ir/instructions.h"
+
+#include "../utils/hash.h"
 
 /**
  * Parses QuickAssembly instructions.
@@ -58,5 +61,65 @@ void parseQAsmInstructions(IR_FUNCTION func, char* buff, int size) {
  * Parses a QuickAssembly instruction.
  */
 inline IR_INSTRUCTION* parseInstruction(char** buff, int size) {
+    IR_INSTRUCTION* instruction = malloc(sizeof(IR_INSTRUCTION));
 
+    int instructionHash = hashstr(buff[0]);
+
+    // Determines the instruction type based on the string hash.
+    switch(instructionHash) {
+        case 2985:
+            instruction->opCode = BLOCK_SWAP;
+            break;
+        case 2987:
+            instruction->opCode = COND_BLOCK_SWAP;
+            break;
+        case 3275:
+            instruction->opCode = LOGICAL_BLOCK_SWAP;
+            break;
+        case 1798:
+            instruction->opCode = S_ALLOC;
+            break;
+        case 2887:
+            instruction->opCode = PTR_SET;
+            break;
+        case 2472:
+            instruction->opCode = PTR_LOAD;
+            break;
+        case 452:
+            instruction->opCode = IADD;
+            break;
+        case 1508:
+            instruction->opCode = ISUB;
+            break;
+        case 1636:
+            instruction->opCode = IMUL;
+            break;
+        case 1284:
+            instruction->opCode = IDIV;
+            break;
+        case 1188:
+            instruction->opCode = ICMP;
+            break;
+        case 1350:
+            instruction->opCode = ICMP_H;
+            break;
+        case 1478:
+            instruction->opCode = ICMP_L;
+            break;
+        case 3272:
+            instruction->opCode = PRM_PUSH;
+            break;
+        case 3144:
+            instruction->opCode = RET_PUSH;
+            break;
+        case 772:
+            instruction->opCode = CALL;
+            break;
+        case 1283:
+            instruction->opCode = RET;
+            break;
+        default:
+            printf("Error: Unknown instruction %s!\n", buff[0]);
+            return NULL;
+    }
 }
