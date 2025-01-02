1537c155d938fa775f07ede02c514a6effee8c06
Zffu <radi0o0@outlook.com>
2025-01-02 15:01:16 +0100

    feat: added parseInt32

---
+++ src/qasm/parser/values.h
@@ -0,0 +1,16 @@
+/**
+ * QuickAssembly Value types parsing.
+ */
+
+#ifndef QASM_PARSER_VALUES_H
+#define QASM_PARSER_VALUES_H
+
+/**
+ * Parses a 32-bit integer into bytes.
+ * @param buff the buff to append the bytes to.
+ * @param startIndex the starting index of where to append the bytes.
+ * @param str the string containing the integer.
+ */
+void parseInt32(unsigned char* buff, int startIndex, char* str);
+
+#endif
