72be49fd2fdec98eabba04e9e8ef303bf1744a54
Zffu <radi0o0@outlook.com>
2025-01-08 16:46:08 +0100

    feat: added very bare bones writer (wip)

---
+++ src/qasm/writer/writer.c
@@ -0,0 +1,18 @@
+/**
+ * The writer of QuickAssembly. The inline assembly of Quickfall.
+ */
+
+#include <stdio.h>
+
+#include "../../ir/structs.h"
+
+/**
+ * Writes some standart QuickAssembly code equivalent to the parsed IR.
+ * @param fptr the file output.
+ * @param output the output of the IR convertor.
+ */
+void writeQASM(FILE* fptr, IR_OUTPUT* output) {
+    for(int i = 0; i < output->blockCount; ++i) {
+        fprintf(fptr, "%d:\n", i);
+    }
+}
