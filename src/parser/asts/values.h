e4cc688b8826b982ebc5888e309dd2b9e118d378
Zffu <radi0o0@outlook.com>
2025-01-09 12:52:37 +0100

    feat: added multi type handling for ast value parsing

--- src/parser/asts/values.h
+++ src/parser/asts/values.h
@@ -6,6 +6,8 @@
 
 #include "../../lexer/lexer.h"
 
+#include "../../lib/types.h"
+
 #ifndef VALUES_AST_H
 #define VALUES_AST_H
 
@@ -13,14 +15,16 @@
  * Parses an AST value on the lexer result.
  * @param result the Lexer result.
  * @param index the index of the start of the parsing.
+ * @param exceptedType The excepted type of token.
  */
-AST_VALUE* parseASTValue(LEXER_RESULT result, int index);
+AST_VALUE* parseASTValue(LEXER_RESULT result, int index, LIB_TYPES exceptedType);
 
 /**
  * Parses an actual expression as value instead of just one token.
  * @param result the Lexer result.
  * @param index the index of the start of the parsing.
+ * @param exceptedType The excepted type of token.
  */
-void* parseValueGroup(LEXER_RESULT result, int index);
+void* parseValueGroup(LEXER_RESULT result, int index, LIB_TYPES exceptedType);
 
 #endif
